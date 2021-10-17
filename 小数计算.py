a4d1ddbeebe6b66023edce7ab6f7ba8459f08b43
黄闻天 HwangWentian <85811921+HwangWentian@users.noreply.github.com>
2021-10-17 11:54:48 +0800

    添加了乘方运算函数

--- 小数计算.py
+++ 小数计算.py
@@ -18,8 +18,7 @@ def add(a1: int, b1: int, a2: int, b2: int) -> tuple:
     a2 *= gcd_ // b2
     returns = [a1 + a2, gcd_]
     gcd_ = gcd(returns[0], returns[1])
-    returns = (returns[0] // gcd_, returns[1] // gcd_)
-    return returns
+    return returns[0] // gcd_, returns[1] // gcd_
 
 
 def subtract(a1: int, b1: int, a2: int, b2: int) -> tuple:
@@ -35,8 +34,7 @@ def subtract(a1: int, b1: int, a2: int, b2: int) -> tuple:
     a2 *= gcd_ // b2
     returns = [a1 - a2, gcd_]
     gcd_ = gcd(returns[0], returns[1])
-    returns = (returns[0] // gcd_, returns[1] // gcd_)
-    return returns
+    return returns[0] // gcd_, returns[1] // gcd_
 
 
 def multiply(a1: int, b1: int, a2: int, b2: int) -> tuple:
@@ -49,8 +47,7 @@ def multiply(a1: int, b1: int, a2: int, b2: int) -> tuple:
     """
     returns = [a1 * a2, b1 * b2]
     gcd_ = gcd(returns[0], returns[1])
-    returns = (returns[0] // gcd_, returns[1] // gcd_)
-    return returns
+    return returns[0] // gcd_, returns[1] // gcd_
 
 
 def divide(a1: int, b1: int, a2: int, b2: int) -> tuple:
@@ -63,8 +60,26 @@ def divide(a1: int, b1: int, a2: int, b2: int) -> tuple:
     """
     returns = [a1 * b2, b1 * a2]
     gcd_ = gcd(returns[0], returns[1])
-    returns = (returns[0] // gcd_, returns[1] // gcd_)
-    return returns
+    return returns[0] // gcd_, returns[1] // gcd_
 
 
-print(divide(-1, 10, 2, 5))
+def power(a1: int, a2: int, b: int) -> tuple:
+    """
+    :param a1: 底数的分子
+    :param a2: 底数的分母
+    :param b: 指数（为整数）
+    :return: 运算结果的分子和分母，形式为 ({分子}, {分母})
+    """
+    n = 1
+    m = 1
+    for i in range(abs(b)):
+        n *= a1
+        m *= a2
+    gcd_ = gcd(n, m)
+    if b < 0:
+        return m // gcd_, n // gcd_
+    else:
+        return n // gcd_, m // gcd_
+
+
+print(power(3, 1, -2))
